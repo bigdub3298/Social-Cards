2b46d12fe1a29c7fed80743dc56f050d627032d0
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-18 22:37:56 -0700

    styled components

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,6 +5,7 @@ export default function Avatar({ image }) {
   return (
     <div className="avatar">
       <img src={image} alt="avatar" className="avatar__img" />
+      <div className="avatar__overlay"></div>
     </div>
   );
 }
