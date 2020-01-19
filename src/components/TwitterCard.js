426ba5b20cd56f340ddc9314449391eb595c410f
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-18 22:36:22 -0700

    added twitter card body and image

--- src/components/TwitterCard.js
+++ src/components/TwitterCard.js
@@ -1,20 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import faker from "faker";
 import Header from "./Header";
 import Avatar from "./Avatar";
 import "../styles/TwitterCard.scss";
 
-export class TwitterCard extends Component {
-  render() {
-    return (
-      <div className="twitter-card">
-        <div className="twitter-card__header">
-          <Avatar image={faker.image.avatar()} />
-          <Header />
-        </div>
+export default function TwitterCard(props) {
+  return (
+    <div className="twitter-card">
+      <div className="twitter-card__header">
+        <Avatar image={faker.image.avatar()} />
+        <Header />
       </div>
-    );
-  }
+      <div className="twitter-card__body">
+        <div className="twitter-card__body-text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          aspernatur odit aperiam. Earum consequuntur dicta dolores voluptates,
+          ipsa velit fugiat iure a perspiciatis officiis deserunt accusantium
+          delectus nemo reiciendis consequatur animi. Doloribus assumenda
+          aliquam minima nostrum. Veniam accusantium rem minima fugit? Error
+          aliquid expedita saepe temporibus odit asperiores. Vitae, laborum.
+        </div>
+        <img
+          src={faker.image.food()}
+          alt=""
+          className="twitter-card__body-image"
+        />
+      </div>
+    </div>
+  );
 }
-
-export default TwitterCard;
